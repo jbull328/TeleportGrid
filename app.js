abc79ebe07f37870ca48323b665a17d6a84b7bfb
jbull328 <jackbull328@gmail.com>
2018-02-18 23:08:56 -0800

    added sorting for all the different datatypes

--- app.js
+++ app.js
@@ -1,13 +1,58 @@
 
 const fs = require('fs');
 
+
+//----------Pulls Data from Text File-----------------//
+
+
 fs.readFile('input.txt', (err, data) => {
     if(err) throw err;
     let inputData = data.toString().split("\n");
     for(i in inputData) {
-        console.log(inputData[i]);
     }
+  
+  //Splits Cities and Queries into their own arrays//
+    
+  let cities = [];
+  let distanceQueires = [];
+  let pointToPointQuerires = [];
+  let loopQueries = [];
+
+  let regexCity = /-/g;
+  let regexDist = /cities/;
+  let regexPointToPoint = /can/;
+  let regexLoop = /loop/;
+  
 
 
+  inputData.forEach(function(element, index){ 
+    if(element.match(regexCity) != null){ 
+       cities.push(element);  
+    } else if (element.match(regexDist) != null){
+        distanceQueires.push(element);
+    } else if (element.match(regexPointToPoint) != null){
+      pointToPointQuerires.push(element);
+    } else if (element.match(regexLoop) != null){
+      loopQueries.push(element);
+    } 
+    });
 
+ console.log("--------------------> " + cities + " From cities array");
+ console.log("--------------------> " + distanceQueires + " From distance queries")
+ console.log("--------------------> " + pointToPointQuerires + " From point to array");
+ console.log("--------------------> " + loopQueries + " From Loop queries");
+
+ //Begins to split up cities to a more usuable structure
+
+ let citiesLinked = [];
+ let regexLastCity = /$(\w+)\$$/;
+     
+ cities.forEach((element, index) => {
+  
+  element.split(" ");
+  citiesLinked.push
+ });
+
+ console.log("------------------> " + citiesLinked + " From cities linked");
 });
+
--- test/test.js
+++ test/test.js
@@ -1,10 +1,17 @@
 const expect    = require("chai").expect;
 const inputData = require("../app.js");
+const cities = require("../app.js");
 
 describe("Pull Data From Text File", function() {
   describe("Insert Data into an Array", function() {
     it("Outputs an Array of Data", function() {
       expect(inputData).to.equal!=null;
     });
+  });
+});
+
+describe("Sorts Cites into its own array", function() {
+  if("Outpusts an array of cities", function() {
+    expect(cities).to.equal!=null;
   });
 });
